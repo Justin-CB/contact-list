d2ed8e152b0252a8996526623ad5036c7d1ae161
ErakiJeff <erakiproj99@gmail.com>
2023-09-12 14:38:53 -0400

    updated personal contact (what is a Label????)

--- PersonalContact.java
+++ PersonalContact.java
@@ -1,3 +1,57 @@
 public class PersonalContact extends Contact {
     private Label label; // Label for grouping personal contacts
+
+    // private values = new String[] {"phone", "status", "first", "last", "email",
+    // "street", "city", "state", "zip", "title", "department", "company", "label"};
+    private String[] attrs = new String[] { "label" };
+
+    @Override
+    public boolean exists(String attribute) {
+        if (super.exists(attribute)) {
+            return true;
+        }
+
+        for (String attr : attrs) {
+            if (attribute.equals(attr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean hasValue(String attribute, String value) throws IllegalArgumentException {
+        boolean containsValue = false;
+        try {
+            containsValue = super.hasValue(attribute, value);
+        } catch (IllegalArgumentException e) {
+            value = value.toLowerCase();
+            switch (attribute) {
+                case "label":
+                    // TODO This does not make sense because I don't know what a
+                    // label is
+                    containsValue = label.equals(value);
+                default:
+                    throw e;
+            }
+        }
+        return containsValue;
+    }
+
+    @Override
+    public void setValue(String attribute, String value) throws IllegalArgumentException {
+        try {
+            super.setValue(attribute, value);
+        } catch (IllegalArgumentException e) {
+            value = value.toLowerCase();
+            switch (attribute) {
+                case "label":
+                    // TODO This also does not make sense because I don't know
+                    // what a label is
+                    label = new Label(value);
+                default:
+                    throw e;
+            }
+        }
+    }
 }
--- PersonalInfo.java
+++ PersonalInfo.java
@@ -13,43 +13,6 @@ public class PersonalInfo implements Cloneable {
         return String.format("%s, %s: (%s)", first, last, status.toString());
     }
 
-    /* Object's .clone() should work for this */
-
-    private enum Status {
-        MARRIED,
-        SINGLE,
-        DIVORCED,
-        WIDOWED,
-        NA;
-
-        /**
-         * The toString method for Status
-         * 
-         * @return the string representation of the marital status
-         */
-        public String toString() {
-            if (this == Status.NA) {
-                return "N/A";
-            } else {
-                /*
-                 * Leave the 1st character capital & make the rest lower *
-                 * case i.e. change MARRIED into Married.
-                 */
-                return name().substring(0, 1) +
-                        name().substring(1).toLowerCase();
-            }
-        }
-
-        public static Status fromString(String str) {
-            for (int i = 0; i < Status.values().length; i++) {
-                if (str.toUpperCase().equals(Status.values()[i].name())) {
-                    return Status.values()[i];
-                }
-            }
-            return Status.NA;
-        }
-    }
-
     public String getFirst() {
         return first;
     }
