84eaf7e1a6289800625bd95f6b15bb562af8f6f6
kevinmccall <erakiproj99@gmail.com>
2023-09-17 10:52:30 -0400

    commented Node.java

--- Node.java
+++ Node.java
@@ -1,7 +1,21 @@
+/**
+ * @author Justin Bester and Kevin McCall
+ * @version 1.0
+ * 
+ *          Node for creating a linked list
+ */
 public class Node<T> {
+    /** The next node for a linked list */
     public Node<T> next;
+    /** the data being stored on a Node */
     public T data;
 
+    /**
+     * Creates a new node.
+     * 
+     * @param data the data of this node.
+     * @param next the next node.
+     */
     public Node(T data, Node<T> next) {
         this.data = data;
         this.next = next;
